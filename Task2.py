f79b680f1bdb29f5119548a14ea1f311ff0975ee
samsonowal <74383977+samsonowal@users.noreply.github.com>
2025-07-04 09:47:35 +0530

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,7 @@
+import math
+n=int(input("Enter a number: "))
+e=2.71828
+
+print("Square root of the number",n,"is:",math.sqrt(n))
+print("Natural logarithm (log base e) of the number",n,"is:",math.log(n,e))
+print("Sine of the number (in radians)",n,"is:",math.sin(n))
